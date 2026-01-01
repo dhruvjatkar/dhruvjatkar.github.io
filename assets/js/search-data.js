8b1713173956c2f40016aaffe509415d0ec2943f
dhruvjatkar <51522517+dhruvjatkar@users.noreply.github.com>
2026-01-01 11:47:04 +0000

    Deploying to gh-pages from @ dhruvjatkar/dhruvjatkar.github.io@41e266eb0f373884ebb0e9468b7c21f33d61ce25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -476,11 +476,25 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
